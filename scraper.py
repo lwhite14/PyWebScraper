362051044f66a501e3c0a3d542173b88af3309b4
Luke White <luke_white@yahoo.com>
2023-04-27 19:37:37 +0100

    Command line arguments to break up different uni scraping

---
+++ scraper.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+
+class Scraper():
+    def __init__(self):
+        print ("Init")
+
+    def Manchester(self):
+        print ("Scrape for manchester")
+
+    def Birmingham(self):
+        print ("Scrape for birmingham")
+
+    def Surrey(self):
+        print ("Scrape for surrey")
